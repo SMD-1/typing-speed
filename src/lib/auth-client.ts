3d03063f4b79d4341c9dd596779f70b0d3e1b31c
smd-1 <skdanish1999@gmail.com>
2025-04-08 12:37:22 +0530

    add credentials include in auth client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,4 +2,7 @@ import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   /** the base url of the server (optional if you're using the same domain) */
   baseURL: import.meta.env.VITE_API_BASE_URL,
+  fetchOptions: {
+    credentials: "include",
+  },
 });
--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import Login from "./components/Login.tsx";
 import { ThemeProvider } from "./context/ThemeContext.tsx";
 import { Signup } from "./components/Signup.tsx";
+import Leaderboard from "./components/Leaderboard.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -15,6 +16,7 @@ createRoot(document.getElementById("root")!).render(
           <Route path="/" element={<App />} />
           <Route path="/login" element={<Login />} />
           <Route path="/create-account" element={<Signup />} />
+          <Route path="/leaderboard" element={<Leaderboard />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
